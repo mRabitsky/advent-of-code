1b71aee62979bd86768cc1191e0630ce670807f7
Matt <mrabitsky@gmail.com>
2020-11-30 22:41:37 -0800

    first day of 2020!!!
    Added some boilerplate for Python and also did day one. Andy's friend got #8 on the global leaderboards but then they invalidated it because the server broke.

---
+++ 2020/python/main.py
@@ -0,0 +1,14 @@
+import sys
+
+from days import day1
+
+days = [[day1.part1, day1.part2]]
+
+if __name__ == '__main__':
+    day: int = int(sys.argv[1].strip()) - 1
+    part: str = sys.argv[2]
+    if part == 'both':
+        print(days[day][0]())
+        print(days[day][1]())
+    else:
+        print(days[day][int(part.strip()) - 1]())
