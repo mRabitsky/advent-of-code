e8378967e724e4e3b3dd3b7cdd87233f152feb90
Matt <mrabitsky@gmail.com>
2020-12-02 22:31:48 -0800

    --- Day 3: Toboggan Trajectory ---
    Andy said this code was clean! :flushed:

--- 2020/python/main.py
+++ 2020/python/main.py
@@ -1,8 +1,8 @@
 import sys
 
-from days import day1, day2
+from days import day1, day2, day3
 
-days = [[day1.part1, day1.part2], [day2.part1, day2.part2]]
+days = [[day1.part1, day1.part2], [day2.part1, day2.part2], [day3.part1, day3.part2]]
 
 if __name__ == '__main__':
     day: int = int(sys.argv[1].strip()) - 1
