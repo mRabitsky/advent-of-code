09d821f4c9da028ef4a42e86d2c20e1978fdba05
Matt <mrabitsky@gmail.com>
2020-12-07 00:57:50 -0800

    --- Day 7: Handy Haversacks ---
    I really wanted there to be a fancy adjacency matrix computation for part two but I just couldn't wrap my head around it so I ended up doing the boring thing instead.

--- 2020/python/main.py
+++ 2020/python/main.py
@@ -1,13 +1,14 @@
 import sys
 
-from days import day1, day2, day3, day4, day5, day6
+from days import day1, day2, day3, day4, day5, day6, day7
 
 days = [[day1.part1, day1.part2],
         [day2.part1, day2.part2],
         [day3.part1, day3.part2],
         [day4.part1, day4.part2],
         [day5.part1, day5.part2],
-        [day6.part1, day6.part2]]
+        [day6.part1, day6.part2],
+        [day7.part1, day7.part2]]
 
 if __name__ == '__main__':
     day: int = int(sys.argv[1].strip()) - 1
