c264757aaefd5c5c6bc6b545e2f7746157eed3bc
Matt <mrabitsky@gmail.com>
2020-12-08 22:59:25 -0800

    --- Day 9: Encoding Error ---
    Okay so you can't use the inchworm sliding window approach since the input isn't sorted, but I wrote a variant of the inchworm that I'm calling the fencer which does work for this unsorted input.

--- 2020/python/main.py
+++ 2020/python/main.py
@@ -1,6 +1,6 @@
 import sys
 
-from days import day1, day2, day3, day4, day5, day6, day7, day8
+from days import day1, day2, day3, day4, day5, day6, day7, day8, day9
 
 days = [[day1.part1, day1.part2],
         [day2.part1, day2.part2],
@@ -9,7 +9,8 @@ days = [[day1.part1, day1.part2],
         [day5.part1, day5.part2],
         [day6.part1, day6.part2],
         [day7.part1, day7.part2],
-        [day8.part1, day8.part2]]
+        [day8.part1, day8.part2],
+        [day9.part1, day9.part2]]
 
 if __name__ == '__main__':
     day: int = int(sys.argv[1].strip()) - 1
