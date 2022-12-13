9132e9dadd7386ac2d81be0a7a2330db131e6843
mdr <matt@rabitsky.dev>
2022-12-13 02:04:31 -0300

    --- Day 12: Hill Climbing Algorithm ---
    God bless the pixel graph function.

--- 2022/python/days/__init__.py
+++ 2022/python/days/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['day1', 'day2', 'day3', 'day4', 'day5', 'day6', 'day7', 'day8', 'day9', 'day10', 'day11']
+__all__ = ['day1', 'day2', 'day3', 'day4', 'day5', 'day6', 'day7', 'day8', 'day9', 'day10', 'day11', 'day12']
---
+++ 2022/python/days/day12.py
@@ -0,0 +1,76 @@
+from math import nan
+
+import numpy as np
+from scipy.sparse.csgraph import shortest_path
+from skimage.graph import pixel_graph
+
+grid = '''
+abccccaaaaaaacccaaaaaaaccccccccccccccccccccccccccccccccccaaaa
+abcccccaaaaaacccaaaaaaaaaaccccccccccccccccccccccccccccccaaaaa
+abccaaaaaaaaccaaaaaaaaaaaaaccccccccccccccccccccccccccccaaaaaa
+abccaaaaaaaaaaaaaaaaaaaaaaacccccccccaaaccccacccccccccccaaacaa
+abaccaaaaaaaaaaaaaaaaaacacacccccccccaaacccaaaccccccccccccccaa
+abaccccaaaaaaaaaaaaaaaacccccccccccccaaaaaaaaaccccccccccccccaa
+abaccccaacccccccccaaaaaacccccccccccccaaaaaaaacccccccccccccccc
+abcccccaaaacccccccaaaaaaccccccccijjjjjjaaaaaccccccaaccaaccccc
+abccccccaaaaacccccaaaacccccccciiijjjjjjjjjkkkkkkccaaaaaaccccc
+abcccccaaaaacccccccccccccccccciiiirrrjjjjjkkkkkkkkaaaaaaccccc
+abcccccaaaaaccccccccccccccccciiiirrrrrrjjjkkkkkkkkkaaaaaccccc
+abaaccacaaaaacccccccccccccccciiiqrrrrrrrrrrssssskkkkaaaaacccc
+abaaaaacaaccccccccccccccccccciiiqqrtuurrrrrsssssskklaaaaacccc
+abaaaaacccccccccccaaccccccccciiqqqttuuuurrssusssslllaaccccccc
+abaaaaaccccccccaaaaccccccccciiiqqqttuuuuuuuuuuusslllaaccccccc
+abaaaaaacccccccaaaaaaccccccciiiqqqttxxxuuuuuuuusslllccccccccc
+abaaaaaaccccaaccaaaaacccccchhiiqqtttxxxxuyyyyvvsslllccccccccc
+abaaacacccccaacaaaaaccccccchhhqqqqttxxxxxyyyyvvsslllccccccccc
+abaaacccccccaaaaaaaacccccchhhqqqqtttxxxxxyyyvvssqlllccccccccc
+abacccccaaaaaaaaaaccaaacchhhpqqqtttxxxxxyyyyvvqqqlllccccccccc
+SbaaacaaaaaaaaaaaacaaaaahhhhppttttxxEzzzzyyvvvqqqqlllcccccccc
+abaaaaaaacaaaaaacccaaaaahhhppptttxxxxxyyyyyyyvvqqqlllcccccccc
+abaaaaaaccaaaaaaaccaaaaahhhppptttxxxxywyyyyyyvvvqqqmmcccccccc
+abaaaaaaacaaaaaaacccaaaahhhpppsssxxwwwyyyyyyvvvvqqqmmmccccccc
+abaaaaaaaaaaaaaaacccaacahhhpppssssssswyyywwvvvvvqqqmmmccccccc
+abaaaaaaaacacaaaacccccccgggppppsssssswwywwwwvvvqqqqmmmccccccc
+abcaaacaaaccccaaaccccccccgggppppppssswwwwwrrrrrqqqmmmmccccccc
+abcaaacccccccccccccccccccgggggpppoosswwwwwrrrrrqqmmmmddcccccc
+abccaacccccccccccccccccccccgggggoooosswwwrrrnnnmmmmmddddccccc
+abccccccccccccccccccccccccccgggggooossrrrrrnnnnnmmmddddaccccc
+abaccccaacccccccccccccccccccccgggfoossrrrrnnnnndddddddaaacccc
+abaccaaaaaaccccccccccccccccccccgffooorrrrnnnneeddddddaaaacccc
+abaccaaaaaacccccccccccccccccccccfffooooonnnneeeddddaaaacccccc
+abacccaaaaaccccccccaaccaaaccccccffffoooonnneeeeccaaaaaacccccc
+abcccaaaaacccccccccaaccaaaaccccccffffoooneeeeeaccccccaacccccc
+abaccaaaaaccccccccaaaacaaaaccccccafffffeeeeeaaacccccccccccccc
+abacccccccccccccccaaaacaaacccccccccffffeeeecccccccccccccccaac
+abaaaacccccccaaaaaaaaaaaaaacccccccccfffeeeccccccccccccccccaaa
+abaaaacccccccaaaaaaaaaaaaaaccccccccccccaacccccccccccccccccaaa
+abaacccccccccaaaaaaaaaaaaaaccccccccccccaacccccccccccccccaaaaa
+abaaaccccccccccaaaaaaaaccccccccccccccccccccccccccccccccaaaaaa
+'''.strip().splitlines()
+grid = np.asarray([list(map(ord, row)) for row in grid], dtype=int)
+s = np.argwhere(grid == ord('S'))[0]
+e = np.argwhere(grid == ord('E'))[0]
+grid[tuple(s)] = ord('a')
+grid[tuple(e)] = ord('z')
+grid -= (ord('a') - 1)
+[s, e] = np.ravel_multi_index(np.asarray([s, e]).T, grid.shape)
+
+
+def _edge(ps, ns, _):
+    return [1 if p - n >= -1 else nan for (p, n) in zip(ps, ns)]
+
+
+def part1():
+    return shortest_path(
+        pixel_graph(grid, mask=np.ones_like(grid, dtype=bool), edge_function=_edge)[0],
+        directed=True,
+        indices=[s],
+    )[0][e]
+
+
+def part2():
+    return min(x[e] for x in shortest_path(
+        pixel_graph(grid, mask=np.ones_like(grid, dtype=bool), edge_function=_edge)[0],
+        directed=True,
+        indices=np.ravel_multi_index(np.argwhere(grid == 1).T, grid.shape),
+    ))
